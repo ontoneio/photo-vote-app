aa8a90015a0825a43010948bf52543eaf7d0e545
lilvina <davinaleo2007@yahoo.com>
2017-11-15 16:47:14 -0800

    working on queries

--- db/connect-db.js
+++ db/connect-db.js
@@ -2,3 +2,15 @@ const pgp = require('pg-promise')()
 
 const connectionString = "postgres://process.env.USER:@localhost:5432/photos"
 const db = pg(connectionString)
+
+const addLike = (photo) => {
+  
+}
+
+const addDislike = (photo) => {
+
+}
+
+const totalLikes = () => {
+
+}
