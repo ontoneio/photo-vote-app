06149ed2235201a59b22660d88e15c85475698b8
lilvina <davinaleo2007@yahoo.com>
2017-11-16 14:30:18 -0800

    worked on queries for photos

--- db/connect-db.js
+++ db/connect-db.js
@@ -4,13 +4,15 @@ const connectionString = "postgres://process.env.USER:@localhost:5432/photos"
 const db = pg(connectionString)
 
 const addLike = (photo) => {
-  
+  return db.none(`UPDATE photo_votes SET dislikes = (SELECT dislikes FROM photo_votes WHERE photo_id = $1) + 1
+    WHERE photo_id = $1`, photo)
 }
 
 const addDislike = (photo) => {
-
+  return db.none(`UPDATE photo_votes SET likes = (SELECT likes FROM photo_votes WHERE photo_id = $1) + 1
+    WHERE photo_id = $1`, photo)
 }
 
-const totalLikes = () => {
-
+const getLikes = (photo) => {
+  return db.any(`SELECT * FROM photo_votes WHERE photo_id = $1`, photo)
 }
